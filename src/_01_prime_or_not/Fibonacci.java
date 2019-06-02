668bb990ed5c11da9e9cebd4e6c32f6f3288bcc2
league <league@imac-56.attlocal.net>
2019-06-01 17:27:25 -0700

    I did Fibicci and Prime or not! sunglasses

---
+++ src/_01_prime_or_not/Fibonacci.java
@@ -0,0 +1,81 @@
+package _01_prime_or_not;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	
+
+int Bob = 0;
+
+int John = 1;
+
+int Bill = 0;
+
+for (int i = 0; i < 14; i++) {
+	System.out.println(Bob);
+
+	John = John + Bob;
+	
+	Bill = John;
+
+John = Bob;
+
+Bob = Bill;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
