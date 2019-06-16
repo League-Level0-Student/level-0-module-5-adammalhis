58aa11efe9ab1da48226711f088e865d140b5f9e
league <league@imac-56.attlocal.net>
2019-06-15 17:25:33 -0700

    I did for loop gaunbtlet and Skill practice still in progress!

---
+++ src/_05_circle_ring/forloopgauntlet.java
@@ -0,0 +1,117 @@
+package _05_circle_ring;
+
+public class forloopgauntlet {
+public static void main(String[] args) {
+	
+for (int i = 0; i < 100; i++) {
+	System.out.println(i);
+}
+for (int i = 100; i > 0; i--) {
+	System.out.println(i);
+}
+	for (int i = 2; i <= 100; i++) {
+		if (i % 2 == 0) {
+			
+			System.out.println(i);
+		}
+	}
+	for (int i = 1; i <= 99; i++) {
+	if(i%2 != 0) {
+		System.out.println(i);
+	}
+	}
+	for (int i = 1; i <= 500; i++) {
+		if (i%2 == 0) {
+			System.out.println(i+"even");
+		}
+		else {
+			System.out.println(i+"odd");
+		}
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
